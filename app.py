ff14287b5dc6e668a2949fe5f634e1f35af73571
Jobinjoy Ponnappal <106422383+jobinjoy12@users.noreply.github.com>
2024-02-23 15:04:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,5 +32,5 @@ if st.button("Predict type of Iris"):
 st.text('')
 st.text('')
 st.markdown(
-    '`Create by` [Intel-Unnati](https://github.com/intel-unnati-saintgits/intel-unnati-certificate-programme/blob/main/README.md) | \
+    '`Create by` [Jobinjoy-Ponnappal](https://github.com/jobinjoy12) | \
          `Code:` [GitHub](https://github.com/intel-unnati-saintgits/Model-Deployment-Demo/edit/main/app.py)')
